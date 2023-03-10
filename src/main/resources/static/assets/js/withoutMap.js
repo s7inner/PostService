76eb62cdebc869228855089b1335507433aad600
“Moisak <skillfull357@gmail.com>
2023-03-10 14:42:54 +0200

    "delete some scripts"

--- src/main/resources/static/assets/js/withoutMap.js
+++ src/main/resources/static/assets/js/withoutMap.js
@@ -37,31 +37,6 @@ function initAutocomplete() {
             distance_field.value = distance_text;
             var duration_field = document.getElementById("duration");
             duration_field.value = duration_text;
-
-
-
-            //     // Display the route on the map
-            //     var map = new google.maps.Map(document.getElementById('map'), {
-            //         zoom: 10,
-            //         center: response.routes[0].legs[0].start_location
-            //     });
-            //     var directionsRenderer = new google.maps.DirectionsRenderer({
-            //         map: map,
-            //         directions: response,
-            //         draggable: true,
-            //         polylineOptions: {
-            //             strokeColor: 'green',
-            //             strokeWeight: 5,
-            //             strokeOpacity: 0.7
-            //         }
-            //     });
-            // } else {
-            //     // Display an error message if the request fails
-            //     var distance_field = document.getElementById("distance");
-            //     distance_field.value = "Error: " + status;
-            //     var duration_field = document.getElementById("duration");
-            //     duration_field.value = "";
-            // }
         }
     });
 }
